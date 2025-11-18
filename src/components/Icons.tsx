e29d0f3c9c352f1a4a36cbbd6e28589793f86858
Jakob Bergström <n.jakob.bergstrom@outlook.com>
2025-11-18 22:49:14 +0100

    react naming conv

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -28,16 +28,16 @@ export function PokemonIcon({
 	return (
 		<svg
 			className={cn(className)}
-			{...props}
 			xmlns='http://www.w3.org/2000/svg'
 			width='24'
 			height='24'
 			viewBox='0 0 24 24'
 			fill='none'
 			stroke='currentColor'
-			stroke-width='2'
-			stroke-linecap='round'
-			stroke-linejoin='round'
+			strokeWidth='2'
+			strokeLinecap='round'
+			strokeLinejoin='round'
+			{...props}
 		>
 			<title>Pokeball</title>
 			<circle cx='12' cy='12' r='10' />
