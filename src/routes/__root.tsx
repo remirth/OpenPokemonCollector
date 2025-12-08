e5944549c048246cb7166cab620033234efbb7d3
remirth <n.jakob.bergstrom@outlook.com>
2025-12-08 14:16:14 +0100

    fix row layout to use flex

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -12,15 +12,15 @@ export const Route = createRootRoute({
 function RootComponent() {
 	return (
 		<>
-			<div className='grid-rows-9 grid h-dvh w-dvw fixed'>
-				<Header className='row-span-1' />
-				<section className='md:row-span-8 row-span-7 w-full grid grid-cols-12'>
+			<div className='flex flex-col h-dvh w-dvw fixed'>
+				<Header className='max-h-[70px]' />
+				<section className='grow w-full grid grid-cols-12'>
 					<AppSidebar className='col-span-2 hidden md:block' />
 					<main className='col-span-full md:col-span-10 overflow-y-auto bg-background bg-[linear-gradient(to_right,#80808033_1px,transparent_1px),linear-gradient(to_bottom,#80808033_1px,transparent_1px)] bg-size-[70px_70px] prose-h4:xl:text-2xl prose-h4:lg:text-xl prose-h4:text-lg h-full pb-16 md:pb-0'>
 						<Outlet />
 					</main>
 				</section>
-				<AppTabs className='md:hidden row-span-1' />
+				<AppTabs className='md:hidden max-h-fit' />
 			</div>
 			<TanStackRouterDevtools position='bottom-right' />
 			<ReactQueryDevtools position='left' buttonPosition='bottom-left' />
