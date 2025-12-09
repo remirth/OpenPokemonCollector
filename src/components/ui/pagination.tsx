a4c341ee6a1a01613f954adf46141220da057891
remirth <n.jakob.bergstrom@outlook.com>
2025-12-09 15:46:33 +0100

    add pagination

---
+++ src/components/ui/pagination.tsx
@@ -0,0 +1,133 @@
+// biome-ignore-all lint/suspicious/noExplicitAny: We need to cast as any
+
+import {Link} from '@tanstack/react-router';
+import {ChevronLeft, ChevronRight, MoreHorizontal} from 'lucide-react';
+
+import type * as React from 'react';
+
+import {buttonVariants} from '~/components/ui/button';
+
+import {cn} from '~/lib/utils';
+
+function Pagination({className, ...props}: React.ComponentProps<'nav'>) {
+	return (
+		<nav
+			data-slot='pagination'
+			aria-label='pagination'
+			className={cn('mx-auto flex w-full justify-center', className)}
+			{...props}
+		/>
+	);
+}
+
+function PaginationContent({className, ...props}: React.ComponentProps<'ul'>) {
+	return (
+		<ul
+			data-slot='pagination-content'
+			className={cn('flex flex-row items-center gap-1', className)}
+			{...props}
+		/>
+	);
+}
+
+function PaginationItem({className, ...props}: React.ComponentProps<'li'>) {
+	return (
+		<li data-slot='pagination-item' className={cn('', className)} {...props} />
+	);
+}
+
+type PaginationLinkProps = Omit<React.ComponentProps<typeof Link>, 'search'> & {
+	search?: unknown;
+	isActive?: boolean;
+	size?: 'default' | 'sm' | 'lg' | 'icon';
+	disabled?: boolean;
+};
+
+function PaginationLink({
+	className,
+	isActive,
+	size = 'icon',
+	disabled,
+	...props
+}: PaginationLinkProps) {
+	return (
+		<Link
+			data-slot='pagination-link'
+			aria-current={isActive ? 'page' : undefined}
+			aria-disabled={disabled || undefined}
+			className={cn(
+				buttonVariants({
+					variant: 'noShadow',
+					size,
+				}),
+				className,
+				disabled && 'bg-secondary-background cursor-not-allowed',
+				isActive && 'bg-black text-white',
+			)}
+			{...(props as any)}
+		/>
+	);
+}
+
+function PaginationPrevious({
+	className,
+	...props
+}: React.ComponentProps<typeof PaginationLink>) {
+	return (
+		<PaginationLink
+			data-slot='pagination-previous'
+			aria-label='Go to previous page'
+			size='default'
+			className={cn('gap-1 pl-2.5', className)}
+			{...props}
+		>
+			<ChevronLeft className='size-4' />
+			<span>Previous</span>
+		</PaginationLink>
+	);
+}
+
+function PaginationNext({
+	className,
+	...props
+}: React.ComponentProps<typeof PaginationLink>) {
+	return (
+		<PaginationLink
+			data-slot='pagination-next'
+			aria-label='Go to next page'
+			size='default'
+			className={cn('gap-1 pr-2.5', className)}
+			{...props}
+		>
+			<span>Next</span>
+			<ChevronRight className='size-4' />
+		</PaginationLink>
+	);
+}
+
+function PaginationEllipsis({
+	className,
+	...props
+}: React.ComponentProps<'span'>) {
+	return (
+		<span
+			data-slot='pagination-ellipsis'
+			aria-hidden
+			className={cn('flex size-9 items-center justify-center', className)}
+			{...props}
+		>
+			<MoreHorizontal className='size-4' />
+			<span className='sr-only'>More pages</span>
+		</span>
+	);
+}
+
+export {
+	Pagination,
+	PaginationContent,
+	PaginationEllipsis,
+	PaginationItem,
+	PaginationLink,
+	PaginationNext,
+	PaginationPrevious,
+};
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,50 +1,113 @@
 import {createFileRoute} from '@tanstack/react-router';
 import {type} from 'arktype';
-import React from 'react';
 import ImageCard from '~/components/ui/image-card';
-import {Input} from '~/components/ui/input';
+import {
+	Pagination,
+	PaginationContent,
+	PaginationEllipsis,
+	PaginationItem,
+	PaginationLink,
+	PaginationNext,
+	PaginationPrevious,
+} from '~/components/ui/pagination';
 import {useEntities} from '~/hooks/useEntities';
 
 const pokedexSearchSchema = type({
-	'page?': 'string.integer',
-	'pageSize?': 'string.integer',
+	'page?': 'number.integer',
+	'pageSize?': 'number.integer',
 });
+
+type PokedexSearch = {
+	page?: number;
+	pageSize?: number;
+};
 
 export const Route = createFileRoute('/')({
 	component: HomeComponent,
-	validateSearch: (s) => pokedexSearchSchema.assert(s),
+	validateSearch: (s: PokedexSearch): PokedexSearch =>
+		pokedexSearchSchema.assert(s),
 });
 
+const api = Route;
+
 function HomeComponent() {
-	const [page, setPage] = React.useState(0);
-	const state = useEntities(Number(page), 50);
+	const search = api.useSearch();
+
+	const pageSize = search.pageSize ?? 50;
+
+	const state = useEntities(search.page, 50);
 
 	return (
-		<>
-			{state.data != null && (
-				<div className='flex flex-row flex-wrap gap-2 align-middle w-full h-fit p-4 pb-16'>
-					<Input
-						value={page}
-						onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-							console.log(e.target.value);
-							setPage(Number(e.target.value ?? '0'));
-						}}
-						type='number'
-						placeholder='Page'
-					></Input>
-					{state.data.map((ent) => (
-						<ImageCard
-							key={ent.id}
-							imageUrl={ent.imageUrl ?? ''}
-							alt={ent.name}
-							caption={`${ent.pokedexNumber}: ${ent.name}`}
-							className='max-w-40'
-						/>
-					))}
-				</div>
-			)}
-			{state.isLoading && 'Loading'}
-			{Boolean(state.error) && state.error?.message}
-		</>
+		<div className='flex flex-row flex-wrap gap-2 align-middle w-full h-fit p-4 pb-16'>
+			<PokedexPagination hasMore={(state.data?.length ?? 0) >= pageSize} />
+			{state.data?.map((ent) => (
+				<ImageCard
+					key={ent.id}
+					imageUrl={ent.imageUrl ?? ''}
+					alt={ent.name}
+					caption={`${ent.pokedexNumber}: ${ent.name}`}
+					className='max-w-40'
+				/>
+			))}
+		</div>
 	);
 }
+
+const PokedexPagination = ({hasMore}: {hasMore: boolean}) => {
+	const search = api.useSearch();
+	const page = search.page ?? 0;
+	const displayPage = page + 1;
+
+	return (
+		<Pagination>
+			<PaginationContent>
+				<PaginationItem>
+					<PaginationPrevious
+						disabled={page === 0}
+						to={api.path}
+						search={{...search, page: page - 1} as unknown}
+					/>
+				</PaginationItem>
+				<PaginationItem>
+					<PaginationLink
+						disabled={page === 0}
+						to={api.path}
+						search={{...search, page: page - 1} as unknown}
+					>
+						{displayPage - 1}
+					</PaginationLink>
+				</PaginationItem>
+				<PaginationItem>
+					<PaginationLink
+						to={api.path}
+						search={{...search, page} as unknown}
+						isActive
+					>
+						{displayPage}
+					</PaginationLink>
+				</PaginationItem>
+				<div className='items-center md:flex hidden'>
+					<PaginationItem>
+						<PaginationLink
+							disabled={!hasMore}
+							to={api.path}
+							search={{...search, page: page + 1} as unknown}
+						>
+							{displayPage + 1}
+						</PaginationLink>
+					</PaginationItem>
+					<PaginationItem>
+						<PaginationEllipsis />
+					</PaginationItem>
+				</div>
+				<PaginationItem>
+					<PaginationNext
+						disabled={!hasMore}
+						to={api.path}
+						search={{...search, page: page + 1} as unknown}
+					/>
+				</PaginationItem>
+			</PaginationContent>
+		</Pagination>
+	);
+};
