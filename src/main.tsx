3cd80f268c1340b1b44cadce5363daa74e14743b
remirth <n.jakob.bergstrom@outlook.com>
2025-12-10 13:10:35 +0100

    include tq dehydration

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,15 @@
 import ReactDOM from 'react-dom/client';
 import './styles.css';
-import {App} from './App';
-import {NotInitializedError} from './lib/errors';
-import {addPersistence} from './persist';
-import {RepositoryContext} from './repositories';
-import {makeQueryClient, makeRouter} from './router';
+import { App } from './App';
+import { NotInitializedError } from './lib/errors';
+import { addPersistence } from './persist';
+import { RepositoryContext } from './repositories';
+import { makeQueryClient, makeRouter } from './router';
 
 // Start repo load as soon as possible
 RepositoryContext.get().catch((e) => {
-	console.error(e);
-	alert('Failed to initialize local database, please reinstall the website!');
+  console.error(e);
+  alert('Failed to initialize local database, please reinstall the website!');
 });
 
 const rootElement = document.getElementById('app');
@@ -20,5 +20,13 @@ const router = makeRouter(queryClient);
 
 await addPersistence(queryClient);
 
+const dehydratedState = (window as any)['__TQ_DEHYDRATED__'] ?? null;
+
 const root = ReactDOM.createRoot(rootElement);
-root.render(<App queryClient={queryClient} router={router} />);
+root.render(
+  <App
+    queryClient={queryClient}
+    router={router}
+    dehydratedState={dehydratedState}
+  />,
+);
